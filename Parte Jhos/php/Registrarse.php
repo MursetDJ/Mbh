38e8a11b3ecbfe535155176bc23b4110a2d05e53
JhosmerGM <86324255+JhosmerGM@users.noreply.github.com>
2021-06-27 12:19:48 -0500

    Pruebas
    pruebas de php

---
+++ Parte Jhos/php/Registrarse.php
@@ -0,0 +1,166 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet"> 
+
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+
+    <link href="Estilos/style-registro.css" rel="stylesheet">
+
+    <title>Formulario de Registro</title>
+  </head>
+  <body>
+      <!-- Para que haya una caja que contenga al registro y la imagen -->
+      <section class="contact-box">
+         <div class="row no-gutters bg-dark">
+
+
+            <!-- Caja de la izquierda -->
+             <div class="col-xl-7 col-lg-12 d-flex ">
+                <div class="container align-self-center p-6">
+                    <h1 class="font-weight-bold mb-3">Formulario de Registro</h1>
+
+                    <!-- <div class="form-group">
+                        <button class="btn btn-outline-dark d-inline-block text-light mr-2 mb-3 width-100"><i class="icon ion-logo-google lead align-middle mr-2"></i> Google </button>
+                        <button class="btn btn-outline-dark d-inline-block text-light mb-3 width-100"><i class="icon ion-logo-facebook lead align-middle mr-2"></i> Facebook</button>
+                    </div> -->
+
+                    <p class="text-muted mb-5">Ingrese la siguiente información para registrarse</p>
+                    <form>
+                        <div class="form-row mb-2">
+                            <div class="form-group col-md-6">
+                                <label class="font-weight-bold">Nombre completo <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control" placeholder="Ingrese su nombre">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label class="font-weight-bold">DNI <span class="text-danger"></span></label>
+                                <input type="text" class="form-control" placeholder="Ingrese DNI">
+                            </div>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label class="font-weight-bold">Fotocheck <span class="text-danger"></span></label>
+                            <input type="text" class="form-control" placeholder="Ingrese Fotocheck">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label class="font-weight-bold">Cargo <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" placeholder="Ingrese Cargo">
+                        </div>
+                        <div class="form-group mb-3 ">
+                            <label class="font-weight-bold">Área de Trabajo <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" placeholder="Ingrese Area de Trabajo">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label class="font-weight-bold">Nombre de Usuario <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" placeholder="Ingrese Usuario">
+                        </div>
+                        <div class="form-group mb-3 ">
+                            <label class="font-weight-bold">Contraseña <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" placeholder="Ingrese contraseña">
+                        </div>
+
+
+                        <div class="form-group mb-5">
+                            <!-- <div class="form-check">
+                                <input class="form-check-input" type="checkbox">
+                                <label class="form-check-label text-muted">Acepto los términos y condiciones</label>
+                            </div> -->
+                        </div>
+                        
+
+                        <button class="btn btn-primary width-100">Regresar</button>
+            
+                        <button class="btn btn-primary width-100">Registrarse</button>
+
+                    </form>
+
+                    <small class="d-inline-block text-muted mt-5"> </small>
+                </div>
+            </div>
+
+
+
+
+
+
+            <!-- Caja de la derecha -->
+            <div class="col-xl-5 col-lg-12 register-bg ">
+                <div class="position-absolute testiomonial p-4">
+                    <!-- <h3 class="font-weight-bold text-light">MBH Capacitaciones - Cajamarca </h3>
+                    <p class="lead text-light">La nueva etapa de la revolución digital se aproxima.</p> -->
+                </div>
+
+
+
+
+
+
+
+                
+             </div>
+         </div> 
+      </section>
+   
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <link rel="stylesheet" href="style.css">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Formulario de Registro</title>
+
+    </head>
+
+    <body>
+        <section class="form-register">
+            <h4>Formulario de Registro</h4>
+
+            <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese su Nombre" >
+            <input class="controls" type="int" name="dni" id="dni" placeholder="Ingrese su DNI" >
+            <input class="controls" type="int" name="fotocheck" id="fotocheck" placeholder="Ingrese su Fotocheck" >
+            <input class="controls" type="text" name="cargo" id="cargo" placeholder="Ingrese su Cargo" >
+            <input class="controls" type="text" name="areaTrabajo" id="areaTrabajo" placeholder="Ingrese su área de trabajo" >
+            <input class="controls" type="text" name="usuario" id="usuario" placeholder="Ingrese su nombre de Usuario" >
+            <input class="controls" type="password" name="contraseña" id="contraseña" placeholder="Ingrese su Contraseña" >
+            <P>Estoy de acuerdo con <a href="#"> Terminos y condiciones</a></P>
+            <input class="botons" type="submit" value="Regresar">
+            <input class="botons" type="submit" value="Resgistrar">
+
+            <p><a href="#"> Ya tengo cuenta </a></p>
+        
+
+        </section>
+    </body>
+
+</html> -->
